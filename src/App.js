e736c5f2d75f65a4cb3037e0ee63e6fd19f44b9b
Petar Milchevski <petar365@gmail.com>
2024-04-22 15:38:27 +0200

    4 Commit

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import WebApp from "./components/Products/WebApp";
 import M_Token from "./components/Products/M_Token";
 import E_Banking from "./components/Products/E-Banking";
 import {Fragment} from 'react';
+import ScrollToTop from "react-scroll-to-top";
 
 const router = createBrowserRouter([
     {
@@ -142,6 +143,12 @@ const router = createBrowserRouter([
 function App() {
     return (
         <div className="App">
+            <ScrollToTop className="goTop"
+                         smooth
+                         color="white"
+                         viewBox="0 0 17 18"
+                         svgPath="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708z
+                         M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
             <RouterProvider router={router}/>
         </div>
     );
--- src/components/Clients.js
+++ src/components/Clients.js
@@ -3,82 +3,65 @@ import '../css/style.css'
 
 function Clients() {
 
-    return (<div>
-        <div className="headers">
+    return (
+        <div>
             <div className="headers">
-                <hr className="invisibleHr"/>
-                <div className="wrapper-content-inner pt-2 pb-2">
-                    <h2><span style={{fontFamily: "Raleway"}}>Наши Клиенти</span></h2>
+                <div className="headers">
+                    <hr className="invisibleHr"/>
+                    <div className="wrapper-content-inner pt-2 pb-2">
+                        <h2><span style={{fontFamily: "Raleway"}}>Наши Клиенти</span></h2>
+                    </div>
+                    <hr className="invisibleHrBottom"/>
                 </div>
-                <hr className="invisibleHrBottom"/>
-            </div>
 
-            <div style={{background:"white"}}>
-                <div className="container px-4 pt-5 pb-5 text-center">
-                    <div className="row gx-5">
-                        <div className="col">
-                            <div className="p-3 clients-img">
-                                <a className="colortobw" href="https://nlb.mk/" target="_blank" rel="noreferrer">
-                                    <img className="img-content" src="/images/NLB%20BANK%20FULL%20COLOR.png"
-                                         alt="NLB Bank"/>
-                                </a>
+                <div style={{background: "white"}}>
+                    <div className="container px-4 pt-5 pb-5 text-center">
+                        <div className="row gx-5">
+                            <div className="col-lg-6 col-md-6 col-sm-6">
+                                <div className="p-lg-3 clients-img">
+                                    <a className="colortobw" href="https://nlb.mk/" target="_blank" rel="noreferrer">
+                                        <img className="img-content" src="/images/NLB%20BANK%20FULL%20COLOR.png"
+                                             alt="NLB Bank"/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-6">
+                                <div className="p-lg-3 clients-img">
+                                    <a className="colortobw" href="https://sparkasse.mk/" target="_blank"
+                                       rel="noreferrer">
+                                        <img className="img-content" src="/images/sparkase_fullcolor-removebg.png"
+                                             alt="SPARKASSE"/>
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                        <div className="col">
-                            <div className="p-3 clients-img">
-                                <a className="colortobw" href="https://sparkasse.mk/" target="_blank" rel="noreferrer">
-                                    <img className="img-content" src="/images/sparkase_fullcolor-removebg.png"
-                                         alt="SPARKASSE"/>
-                                </a>
+                        <hr style={{borderTopColor: "rgba(0, 0, 0, 0.3)"}}/>
+                        <div className="row gx-5 pt-lg-5">
+                            <div className="col-lg-6 col-md-6 col-sm-6">
+                                <div className="p-lg-3 clients-img">
+                                    <a className="colortobw" href="https://silkroadbank.com.mk/" target="_blank"
+                                       rel="noreferrer">
+                                        <img className="img-content" src="/images/silkroad.png"
+                                             alt="Silk Road Bank"/>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <hr style={{borderTopColor: "rgba(0, 0, 0, 0.3)"}}/>
-                    <div className="row gx-5 pt-lg-5">
-                        <div className="col">
-                            <div className="p-3 clients-img">
-                                <a className="colortobw" href="https://silkroadbank.com.mk/" target="_blank" rel="noreferrer">
-                                    <img className="img-content" src="/images/silkroad.png"
-                                         alt="Silk Road Bank"/>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="p-3 clients-img">
-                                <a href="" target="_blank" rel="noreferrer">
-                                    <img className="img-content" src=""
-                                         alt="Open Banking 1"/>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <hr style={{borderTopColor: "rgba(0, 0, 0, 0.3)"}}/>
-
-                    <div className="row gx-5 pt-lg-5">
-
-                        <div className="col">
-                            <div className="p-3 clients-img">
-                                <a href="" target="_blank" rel="noreferrer">
-                                    <img className="img-content" src=""
-                                         alt="Open Banking 2"/>
-                                </a>
+                            <div className="col-lg-6 col-md-6 col-sm-6">
+                                <div className="p-lg-3 clients-img">
+                                    <a className="colortobw" href="https://halkbank.mk/" target="_blank"
+                                       rel="noreferrer">
+                                        <img className="img-content" src="/images/halkbank2.png"
+                                             alt="HalkBank"/>
+                                    </a>
+                                </div>
                             </div>
                         </div>
 
-                        <div className="col">
-                            <div className="p-3 clients-img">
-                                <a href="" target="_blank" rel="noreferrer">
-                                    <img className="img-content" src=""
-                                         alt="Open Banking 3"/>
-                                </a>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>);
+    );
 }
 
 export default Clients;
